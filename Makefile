f5a1a5f119edce3d036a89b4830be6aa47e43a02
Drosinakis Drosos <drososD@proton.me>
2025-11-22 05:11:50 +0200

    create server and docker file for run of the server

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	docker compose -f local.yaml up -d
+build:
+	docker compose -f local.yaml build --no-cache
---
+++ server/config/settings/local.py
@@ -0,0 +1,11 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = (
+    "django-insecure-t07uojdc&4%kumxy!a9@-t-2f&*7b!=0#f=gm7fz+o$##_n5=f"  # noaa
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
