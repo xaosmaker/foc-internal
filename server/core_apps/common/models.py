fa1b34d1899feb60ee688816340ec62fcbb58c2c
Drosinakis Drosos <drososD@proton.me>
2025-11-22 08:45:31 +0200

    remove pycache and start the common App

---
+++ server/core_apps/common/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+    # pkid = models.BigAutoField(primary_key=True, unique=True, editable=False)
+    # id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # edited_at = models.DateTimeField(auto_now=True)
+
+class Location(models.Model):
+
+
